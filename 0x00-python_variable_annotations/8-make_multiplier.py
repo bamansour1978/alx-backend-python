b3379397d56c83f677822b1df7ff736b83a57fed
bamansour1978 <karatman@gmail.com>
2024-01-11 23:41:27 +0100

    8. Complex types - functions

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module documentation"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """function 1 documentation"""
+    def multiplier_func(number: float) -> float:
+        """function 2 documentation"""
+        return number * multiplier
+    return multiplier_func
+    
