35d0c0a2506acdeef3f6736c7b5fd9cbd831aceb
bamansour1978 <karatman@gmail.com>
2024-02-04 22:34:03 +0100

    4. Parameterize and patch as decorators

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-""" Parameterize and patch as decorators, Mocking a property, More patching,
-    Parameterize, Integration test: fixtures, Integration tests """
+"""
+Module documentation
+"""
 import unittest
 from unittest.mock import patch, PropertyMock, Mock
 from parameterized import parameterized
@@ -10,22 +11,27 @@ from urllib.error import HTTPError
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """ TESTCASE """
-    """ inputs to test the functionality """
+    """
+    Class documentation
+    """
     @parameterized.expand([
         ("google"),
         ("abc"),
         ])
     @patch("client.get_json", return_value={"payload": True})
     def test_org(self, org_name, mock_get):
-        """ test that GithubOrgClient.org returns the correct value """
+        """
+        Function documentation
+        """
         test_client = GithubOrgClient(org_name)
         test_return = test_client.org
         self.assertEqual(test_return, mock_get.return_value)
         mock_get.assert_called_once
 
     def test_public_repos_url(self):
-        """ to unit-test GithubOrgClient._public_repos_url """
+        """
+        Fun
+        """
         with patch.object(GithubOrgClient,
                           "org",
                           new_callable=PropertyMock,
@@ -39,7 +45,9 @@ class TestGithubOrgClient(unittest.TestCase):
 
     @patch("client.get_json", return_value=[{"name": "holberton"}])
     def test_public_repos(self, mock_get):
-        """ to unit-test GithubOrgClient.public_repos """
+        """
+        Function  documentation
+        """
         with patch.object(GithubOrgClient,
                           "_public_repos_url",
                           new_callable=PropertyMock,
@@ -50,13 +58,15 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_get.assert_called_once
             mock_pub.assert_called_once
 
-    """ inputs to test the functionality """
+    """parameterized"""
     @parameterized.expand([
         ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False),
         ])
     def test_has_license(self, repo, license_key, expected_return):
-        """ to unit-test GithubOrgClient.has_license """
+        """
+        Function   documentation
+        """
         test_client = GithubOrgClient("holberton")
         test_return = test_client.has_license(repo, license_key)
         self.assertEqual(expected_return, test_return)
